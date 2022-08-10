33653f2543e507548271a89770b23606948154c0
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-08-10 09:30:07 +0530

    [feat]Added navigation and basic structure

--- src/components/work-flow/WorkFlow.jsx
+++ src/components/work-flow/WorkFlow.jsx
@@ -1,7 +1,54 @@
 import styles from "./workflow.module.css";
 import { GiCottonFlower } from "react-icons/gi";
+import { WorkFlowNavigation } from "./components/WorkFlowNavigation";
+import { useState } from "react";
 
 const WorkFlow = ({}) => {
+  const [currPage, setCurrPage] = useState(2);
+
+  const firstPage = {
+    page: 1,
+    title: "Let's set up a home for all your work",
+    subtitle: "You can always create another workspace later.",
+    button: "Create Workspace",
+    onButtonClick: (e) => {},
+    content: (
+      <>
+        <div className={styles.input_wrapper}>
+          <label className={styles.input_label}>Full Name</label>
+          <input
+            placeholder="Steve Jobs"
+            className={styles.input}
+            type="text"
+          />
+        </div>
+        <div className={styles.input_wrapper}>
+          <label className={styles.input_label}>Display Name</label>
+          <input placeholder="Steve" className={styles.input} type="text" />
+        </div>
+      </>
+    ),
+  };
+  const secondPage = {
+    page: 2,
+    title: "Let's set up a home for all your work",
+    subtitle: "You can always create another workspace later.",
+    button: "Create Workspace",
+    onButtonClick: (e) => {},
+    content: (
+      <>
+        <div className={styles.input_wrapper}>
+          <label className={styles.input_label}>Workspace Name</label>
+          <input placeholder="Eden" className={styles.input} type="text" />
+        </div>
+        <div className={styles.input_wrapper}>
+          <label className={styles.input_label}>Workspace URL</label>
+          <input placeholder="Example" className={styles.input} type="text" />
+        </div>
+      </>
+    ),
+  };
+
   return (
     <div className={styles.workflow}>
       <div className={styles.workflow_head}>
@@ -11,8 +58,24 @@ const WorkFlow = ({}) => {
           </div>
           <div className={styles.workflow_heading_text}>Eden</div>
         </div>
+        <div className={styles.workflow_nav}>
+          <WorkFlowNavigation activePage={2} arr={[1, 2, 3, 4]} />
+        </div>
       </div>
-      <div className={styles.workflow_body}></div>
+      <div className={styles.workflow_body}>
+        {[firstPage, secondPage].map((item, index) => {
+          return (
+            <div style={{ display: currPage === item.page ? "block" : "none" }}>
+              <h1 className={styles.workflow_body_title}>{item.title}</h1>
+              <h4 className={styles.workflow_body_subtitle}>{item.subtitle}</h4>
+              {item.content}
+              <button className={styles.button} onClick={item.onButtonClick}>
+                {item.button}
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/components/work-flow/components/WorkFlowNavigation.jsx
@@ -0,0 +1,47 @@
+import styles from "./workflowComponents.module.css";
+
+const WorkFlowNavigation = ({ activePage, arr }) => {
+  return (
+    <div className={styles.navigation_wrapper}>
+      {arr.map((item, i) => (
+        <NavigationBall
+          key={i}
+          index={i + 1}
+          activePage={2}
+          currentIndex={2}
+          lastIndex={arr.length}
+        />
+      ))}
+    </div>
+  );
+};
+
+const NavigationBall = ({ index, activePage, currentIndex, lastIndex }) => {
+  return (
+    <div className={styles.navigation_ball_wrapper}>
+      {index > 1 && (
+        <span
+          className={
+            activePage >= index ? styles.span_left : styles.span_left_empty
+          }
+        ></span>
+      )}
+      <span
+        className={
+          activePage >= index ? styles.span_ball_filled : styles.span_ball_empty
+        }
+      >
+        {index}
+      </span>
+      {index < lastIndex && (
+        <span
+          className={
+            activePage >= index ? styles.span_right : styles.span_right_empty
+          }
+        ></span>
+      )}
+    </div>
+  );
+};
+
+export { WorkFlowNavigation };
